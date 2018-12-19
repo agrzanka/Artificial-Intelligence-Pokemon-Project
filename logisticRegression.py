b6454ee8555f9c503f410a5985bff324fa1f5dc6
cinekpl97 <cinekpl97@gmail.com>
2018-12-19 18:11:36 +0100

    Code refactoring

--- logisticRegression.py
+++ logisticRegression.py
@@ -4,24 +4,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import numpy as np
 
+
 def logistic_regression():
     df = pd.read_csv("Pokemontree.csv")
-    plt.scatter(df.Attack, df.Over,marker ='+', color='red')
-    plt.show()
-    x_train, x_test, y_train, y_test = train_test_split(df[['Attack']],df.Over,test_size=0.1)
-    #print(x_test)
+    # plt.scatter(df.Attack, df.Over, marker ='+', color='red')
+    # plt.show()
+    x_train, x_test, y_train, y_test = train_test_split(df[['Attack']], df.Over,test_size=0.1)
+    print(x_test)
     model = LogisticRegression()
     model.fit(x_train, y_train)
-    yPrediction = model.predict(x_test)
-    # print(yPrediction)
-    score = model.score(x_test,y_test)
-    # print(score)
-    # print( model.predict_proba(x_test))
-    temp = [150]
-    temp = np.array(temp).reshape(-1,1)
-    ile = model.predict(temp)
-    print(ile)
-    print('hahfa')
+
+    y_prediction = model.predict(x_test)
+    print(y_prediction)
+    score = model.score(x_test, y_test)
+    print(score)
+    print(model.predict_proba(x_test))
+
+    single_test = [45]
+    single_test = np.array(single_test).reshape(-1, 1)
+    is_higher_than_1200 = model.predict(single_test)
+    print(is_higher_than_1200)
 
 
 def main():
