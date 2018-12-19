afdfa47e96ede903d8ddd242b1e6151daab77ef7
cinekpl97 <cinekpl97@gmail.com>
2018-12-19 18:32:46 +0100

    End of refactoring

--- logisticRegression.py
+++ logisticRegression.py
@@ -7,24 +7,31 @@ import numpy as np
 
 def logistic_regression():
     df = pd.read_csv("Pokemontree.csv")
+
+        #chart for data given at the begin
     # plt.scatter(df.Attack, df.Over, marker ='+', color='red')
     # plt.show()
+
     x_train, x_test, y_train, y_test = train_test_split(df[['Attack']], df.Over,test_size=0.1)
     print(x_test)
     model = LogisticRegression()
     model.fit(x_train, y_train)
 
-    y_prediction = model.predict(x_test)
-    print(y_prediction)
-    score = model.score(x_test, y_test)
-    print(score)
+    print(model.predict(x_test))
+    print(model.score(x_test, y_test))
     print(model.predict_proba(x_test))
 
     single_test = [45]
     single_test = np.array(single_test).reshape(-1, 1)
-    is_higher_than_1200 = model.predict(single_test)
-    print(is_higher_than_1200)
+    is_higher_than_1200_single = model.predict(single_test)
+    print(is_higher_than_1200_single)
 
+    wide_test = [i + 1 for i in range(200)]
+    wide_test = np.array(wide_test).reshape(-1, 1)
+    is_higher_than_1200_wide = model.predict(wide_test)
+
+    plt.scatter(wide_test, is_higher_than_1200_wide, marker ='+', color='red')
+    plt.show()
 
 def main():
     logistic_regression()
