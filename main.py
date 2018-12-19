64f149408e58a7e2f1009514e8f8462653fc044e
cinekpl97 <cinekpl97@gmail.com>
2018-12-19 15:21:29 +0100

    Added printing prediction

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ def linearPredictionPokemon():
     reg = linear_model.LinearRegression()
     reg.fit(df[['HP', 'Attack', 'Defense']], df.CP)
 
-    y_pred = reg.predict(df[['HP', 'Attack', 'Defense']])
+    yPrediction = reg.predict(df[['HP', 'Attack', 'Defense']])
 
-    print('Variance score: %.2f' % r2_score(df.CP, y_pred))
+    print('Variance score: %.2f' % r2_score(df.CP, yPrediction))
     print(reg.coef_)
     print(reg.intercept_)
+    print(yPrediction)
 
 def main():
     linearPredictionPokemon()
