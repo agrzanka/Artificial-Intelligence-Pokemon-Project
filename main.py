89ea312b86f9e6b2255feec5e6c1391f06a2d1c1
cinekpl97 <cinekpl97@gmail.com>
2018-12-19 15:19:20 +0100

    Clean code, without printing chart, just counting without showing data

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
 import pandas as panda
 from sklearn import linear_model
-from sklearn.metrics import mean_squared_error, r2_score
-from matplotlib import pyplot as plt
-
+from sklearn.metrics import r2_score
 
 def linearPredictionPokemon():
     df = panda.read_csv("Pokemonedit.csv", sep=';')
-    print(df)
     reg = linear_model.LinearRegression()
     reg.fit(df[['HP', 'Attack', 'Defense']], df.CP)
-    print(reg.coef_)
-    print("\n")
-    print(reg.intercept_)
-    y_pred = reg.predict(df[['HP', 'Attack', 'Defense']])
-    meanerror = mean_squared_error(df.CP, y_pred)
-    print('Variance score: %.2f' % r2_score(df.CP, y_pred))
-    # plt.scatter(df['#'], df.CP, color='black')
-    # plt.plot(df['#'], y_pred, color = 'red')
-    #
-    # plt.xticks(())
-    # plt.yticks(())
-    # plt.show()
 
+    y_pred = reg.predict(df[['HP', 'Attack', 'Defense']])
+
+    print('Variance score: %.2f' % r2_score(df.CP, y_pred))
+    print(reg.coef_)
+    print(reg.intercept_)
 
 def main():
     linearPredictionPokemon()
