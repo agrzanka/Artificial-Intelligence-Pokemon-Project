5d6729a3a8a9ce522162dc3ce5dd7f782e7dc1cb
cinekpl97 <cinekpl97@gmail.com>
2018-12-20 13:50:03 +0100

    Added single Linear Regression

---
+++ singleRegression.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn import linear_model
+
+
+def singleregresion():
+    df = pd.read_csv('Pokemontree.csv')
+    HP = df[['HP']]
+    CP = df.CP
+    reg = linear_model.LinearRegression()
+    reg.fit(HP, CP)
+    hp_df = pd.read_csv("hp.csv")
+    p = reg.predict(hp_df)
+    hp_df['CP'] = p
+
+
+def main():
+    singleregresion()
+
+
+if __name__ == "__main__":
+    main()
